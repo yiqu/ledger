847ed3f6f028b2e19a73d380f69c27baa619d7fc
Kevin Qu <kevinqu88@gmail.com>
2023-10-14 22:10:12 -0400

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  plugins: [
+    "mui-path-imports"
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-empty-function": "off",
+    "no-empty": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "mui-path-imports/mui-path-imports": "error",
+    "semi": [
+      1,
+      "always"
+    ],
+    "react/jsx-curly-spacing": [
+      1,
+      "always"
+    ],
+    "no-use-before-define": [
+      0,
+      {
+        "functions": false
+      }
+    ],
+    "react/jsx-indent": [
+      1,
+      2
+    ],
+    "space-in-parens": [
+      1,
+      "never"
+    ],
+    "no-const-assign": [
+      2
+    ],
+    "no-unused-vars": "off",
+    "import/no-duplicates": "off",
+  },
+};
