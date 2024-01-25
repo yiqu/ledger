11e61d7c9a4748bfa1650e41ca040fb2195b9072
Kevin Qu <kevinqu88@gmail.com>
2024-01-24 22:38:06 -0500

    reverted add new time

--- app/shared/utils/time.ts
+++ app/shared/utils/time.ts
@@ -1,6 +1,5 @@
-import formatInTimeZone from 'date-fns-tz/formatInTimeZone';
+import format from "date-fns/format";
 import type { Account } from "../models/account.model";
-import { EST_TIME_ZONE } from "~/api/utils/utils.server";
 
 export function getInitFormValue(type: 'account' | 'expense' | null, account?: Account | null) {
   switch (type) {
@@ -13,7 +12,7 @@ export function getInitFormValue(type: 'account' | 'expense' | null, account?: A
     case 'expense': {
       return {
         amount: '',
-        date: formatInTimeZone(new Date(), EST_TIME_ZONE, 'yyyy-MM-dd HH:mm'),
+        date: format(new Date(), 'yyyy-MM-dd HH:mm'),
         accountId: account?.id || '',
         account: account ?? null,
       };
